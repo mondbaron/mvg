7c954e0f5079de445fe2a0b4063a667aff83fcbb
Martin Dziura <mail@martindziura.de>
2023-08-27 14:09:17 +0000

    apply formatting to test file

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -7,10 +7,10 @@ from mvg import MvgApi, TransportType
 
 def test_basic() -> None:
     """Test: basic usage"""
-    station = MvgApi.station('Universität, München')
-    assert station['id'] == "de:09162:70"
+    station = MvgApi.station("Universität, München")
+    assert station["id"] == "de:09162:70"
     if station:
-        mvgapi = MvgApi(station['id'])
+        mvgapi = MvgApi(station["id"])
         departures = mvgapi.departures()
         assert len(departures) > 0
         print("BASIC: ", station, departures, end="\n\n")
@@ -18,10 +18,10 @@ def test_basic() -> None:
 
 def test_faraway() -> None:
     """Test: basic usage"""
-    station = MvgApi.station('Ebersberg, Ebersberg (Obb.)')
-    assert station['id'] == "de:09175:4070"
+    station = MvgApi.station("Ebersberg, Ebersberg (Obb.)")
+    assert station["id"] == "de:09175:4070"
     if station:
-        mvgapi = MvgApi(station['id'])
+        mvgapi = MvgApi(station["id"])
         departures = mvgapi.departures()
         assert len(departures) > 0
         print("BASIC: ", station, departures, end="\n\n")
@@ -29,10 +29,10 @@ def test_faraway() -> None:
 
 def test_village() -> None:
     """Test: basic usage"""
-    station = MvgApi.station('Egmating, Schule')
-    assert station['id'] == "de:09175:4212"
+    station = MvgApi.station("Egmating, Schule")
+    assert station["id"] == "de:09175:4212"
     if station:
-        mvgapi = MvgApi(station['id'])
+        mvgapi = MvgApi(station["id"])
         departures = mvgapi.departures()
         assert len(departures) > 0
         print("BASIC: ", station, departures, end="\n\n")
@@ -41,20 +41,17 @@ def test_village() -> None:
 def test_nearby() -> None:
     """Test: station by coordinates"""
     station = MvgApi.nearby(48.1, 11.5)
-    assert station['id'] == "de:09162:1480"
+    assert station["id"] == "de:09162:1480"
     print("NEARBY: ", station, end="\n\n")
 
 
 def test_filter() -> None:
     """Test: filters"""
-    station = MvgApi.station('Universität, München')
-    assert station['id'] == "de:09162:70"
+    station = MvgApi.station("Universität, München")
+    assert station["id"] == "de:09162:70"
     if station:
-        mvgapi = MvgApi(station['id'])
-        departures = mvgapi.departures(
-            limit=3,
-            offset=5,
-            transport_types=[TransportType.UBAHN])
+        mvgapi = MvgApi(station["id"])
+        departures = mvgapi.departures(limit=3, offset=5, transport_types=[TransportType.UBAHN])
         assert len(departures) > 0
         print("FILTER: ", station, departures, end="\n\n")
 
@@ -62,9 +59,9 @@ def test_filter() -> None:
 @pytest.mark.asyncio
 async def test_async() -> None:
     """Test: advanced usage with asynchronous methods"""
-    station = await MvgApi.station_async('Universität, München')
-    assert station['id'] == "de:09162:70"
+    station = await MvgApi.station_async("Universität, München")
+    assert station["id"] == "de:09162:70"
     if station:
-        departures = await MvgApi.departures_async(station['id'])
+        departures = await MvgApi.departures_async(station["id"])
         assert len(departures) > 0
         print("ASYNC: ", station, departures, end="\n\n")
